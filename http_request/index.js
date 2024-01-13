b36da371d743dc906aa076f3871ce48c224c097e
Harsika Kumari <kumariharsika8@gmail.com>
2024-01-14 02:22:06 +0530

    learned http request and postman

--- http_request/index.js
+++ http_request/index.js
@@ -3,9 +3,18 @@ const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-    console.log(req.raw);
+    // console.log(req.rawHeaders);
+    res.send("<h1>Hello World !!</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h3>Hello every one welcome to the about page, it is the page made with the express.</h3>");
+});
+
+app.get("/contact", (req, res) => { 
+    res.send("<h2>You can contact me on xxxxxxxx.</h2>")
 });
 
 app.listen(port, () => {
-    // console.log(`The server is running on ${port}`);
+    console.log(`The server is running on ${port}`);
 })
