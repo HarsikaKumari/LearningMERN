f3707def8a1d50b80a4bf157befd0f79a9d3dba9
Harsika Kumari <kumariharsika8@gmail.com>
2024-03-13 20:12:51 +0530

    password change

--- PostgreSQL/World Capital Quiz/index.js
+++ PostgreSQL/World Capital Quiz/index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "Starting",
-  password: "shishir",
+  database: "world",
+  password: "Shishir",
   port: 5432,
 });
 
