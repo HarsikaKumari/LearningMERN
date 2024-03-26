27192be1c92dc3cc1d2fe105415902f6d51d3113
Harsika Kumari <kumariharsika8@gmail.com>
2024-03-27 01:04:15 +0530

    fix(duplicateu ser): One email can only register once

--- AuthenticationLv.1/index.js
+++ AuthenticationLv.1/index.js
@@ -29,20 +29,26 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => { 
+app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
-  const result = await db.query("INSERT INTO users(email, password) VALUES ($1, $2)", [email, password]);
-  console.log(result);
-  res.render("secrets.ejs");
+  const checkResult = db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+  if ((await checkResult).rows.length > 0) {
+    res.send("User already exists. Try logging in!");
+  } else {
+    const result = await db.query("INSERT INTO users(email, password) VALUES ($1, $2)", [email, password]);
+    console.log(result);
+    res.render("secrets.ejs");
+  }
 });
 
 app.post("/login", async (req, res) => {
-const email = req.body.username;
-const password = req.body.password;
-console.log(email);
-console.log(password);
+  const email = req.body.username;
+  const password = req.body.password;
+  console.log(email);
+  console.log(password);
 });
 
 app.listen(port, () => {
