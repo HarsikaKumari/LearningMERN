e3bd32cded7cdff5161b05a59f0a6f3860bfa05d
Harsika Kumari <kumariharsika8@gmail.com>
2024-03-27 20:30:53 +0530

    feat(route): created /login route

--- AuthenticationLv.1/index.js
+++ AuthenticationLv.1/index.js
@@ -33,22 +33,46 @@ app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
-  const checkResult = db.query("SELECT * FROM users WHERE email = $1", [email]);
+  try {
+    const checkResult = db.query("SELECT * FROM users WHERE email = $1", [email]);
 
-  if ((await checkResult).rows.length > 0) {
-    res.send("User already exists. Try logging in!");
-  } else {
-    const result = await db.query("INSERT INTO users(email, password) VALUES ($1, $2)", [email, password]);
-    console.log(result);
-    res.render("secrets.ejs");
+    if ((await checkResult).rows.length > 0) {
+      res.send("User already exists. Try logging in!");
+    } else {
+      const result = await db.query("INSERT INTO users(email, password) VALUES ($1, $2)", [email, password]);
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  }
+  catch (err) {
+    console.log(err);
   }
 });
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-  console.log(email);
-  console.log(password);
+
+  try {
+    const result = db.query("SELECT * FROM users WHERE email = $1", [email,]);
+    console.log(result);
+    
+    if ((await result).rows.length > 0) {
+      const user = result.rows[0];
+      console.log(user);
+      const userPassword = user.password;
+
+      if (password === userPassword) {
+        res.render("secrets.ejs");
+      } else {
+        res.send("Incorrect Password");
+      }
+    } else {
+      res.send("User not found. Try to register!")
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
