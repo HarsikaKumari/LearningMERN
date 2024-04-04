106e07f430d0ea2501764e4b8c43a600a708710a
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-04 15:13:20 +0530

    implemented calculator function using react components

--- ReactJS/LeaningReactJS/src/App.js
+++ ReactJS/LeaningReactJS/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Application from './components/application';
 
-
 function App() {
   return (
     <div> 
--- ReactJS/LeaningReactJS/src/components/application.jsx
+++ ReactJS/LeaningReactJS/src/components/application.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Learning from "./Learning";
+import * as calc from "./calculator.js";
 
 const Application = () => {
+    return (
     <div> 
+        <div>
+            <ul>
+                <li> { calc.default(3, 5)} </li>
+                <li> { calc.subtract(3, 5)} </li>
+                <li> { calc.multiply(3, 5)} </li>
+                <li> { calc.divide(3, 5)} </li>
+            </ul>
+        </div>
         <Learning />
     </div>
+    )
 }
 
 export default Application;
