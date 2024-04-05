49426023dbd3bcaa23b553d463920afb1a2534ef
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-05 22:05:49 +0530

    learing about map function in react

--- ReactJS/react_intermediate/src/components/App.jsx
+++ ReactJS/react_intermediate/src/components/App.jsx
@@ -2,30 +2,29 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 import '../App.css';
+import Avatar from "./Avatar";
+import profile from "../IMG_7954.JPG";
+
+function CreateCard (contacts) {
+  return (
+    <Card
+    id={contacts.id}
+    key={contacts.id}
+    name={contacts.name}
+    img={contacts.imgURL}
+    tel={contacts.phone}
+    email={contacts.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img={profile} />
 
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+     {contacts.map(CreateCard)}
     </div>
   );
 }
