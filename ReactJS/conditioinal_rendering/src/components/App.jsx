aaf75ba12ac20bfd88ef23d0ce7a88e3e71904d6
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-09 22:33:43 +0530

    learning useState component of react

--- ReactJS/conditioinal_rendering/src/components/App.jsx
+++ ReactJS/conditioinal_rendering/src/components/App.jsx
@@ -1,17 +1,43 @@
-import React from "react";
-import Login from "./Login";
-import Register from "./Register";
-
-// let isLoggedIn = false;
-let isUserAuthorized = false;
+import React, { useState } from "react";
+// import Login from "./Login";
+// import Register from "./Register";
+// var isDone = false;
 
 function App() {
+    const [count, setCount] = useState(0);
     return (
-        <div className="container">
-            {/* {isLoggedIn ? <h1>Hello</h1> : <Login /> } */}
-            {isUserAuthorized ? <Login /> : <Register />}
+        <div>
+            <h1>{count}</h1>
+            <button onClick={ () => {
+                setCount(count + 1);
+            }}>+</button>
+
+            <button onClick={ () => {
+                setCount(count - 1);
+            }}>-</button>
         </div>
     );
 }
 
 export default App;
+// let isLoggedIn = false;
+// let isUserAuthorized = false;
+
+// function App() {
+//     return (
+//         <div className="container">
+//             {isLoggedIn ? <h1>Hello</h1> : <Login /> }
+//             {isUserAuthorized ? <Login /> : <Register />}
+//         </div>
+//     );
+// }
+
+/* <p style={isDone && { textDecoration: "line-through" }}> Buy milk </p>
+<button onClick={() => {
+    isDone = false;
+} 
+}> Strike </button>
+ 
+<button onClick={() => {
+    isDone = true;
+}}> UnStrike </button> */
